c89f1aa409ab55fea612e76704f5f2f36358d378
usagisah <usagisah@163.com>
2022-11-01 10:16:39 +0800

    fix: html title & terminal prompt

--- src/normalizeCommand.js
+++ src/normalizeCommand.js
@@ -1,5 +1,5 @@
 export function normalizeCommand(manage, command) {
   return ["yarn", "pnpm"].includes(manage)
     ? `${manage} ${command}`
-    : `${manage} run ${command}`
+    : `${manage} ${command === "install" ? command : `run ${command}`}`
 }
--- src/template/indexHtml.js
+++ src/template/indexHtml.js
@@ -9,7 +9,7 @@ export const indexHtml = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>setsuna.js app</title>
   </head>
   <body>
     <div id="root"></div>
